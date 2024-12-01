c96c72051df9f8af2b3816b914ed542c69c729f2
AtlantaEmrys2002 <milli@Millicents-MacBook-Air.local>
2024-12-01 22:50:03 +0000

    Updating layout of python environment

--- pythonProject/Day1.py
+++ pythonProject/Day1.py
@@ -1,16 +1,16 @@
+# Import Libraries
 import numpy as np
 
+# Read File and Format Input
 file = open('day1input.txt', 'r')
-
 first_line = file.readline().split()
-print(first_line)
 
 list1 = [int(first_line[0])]
 list2 = [int(first_line[1])]
 
 while True:
     first_line = file.readline().split()
-    if first_line == []:
+    if not first_line:
         break
     else:
         line_int = first_line
@@ -18,23 +18,11 @@ while True:
         list1.append(int(line1))
         list2.append(int(line2))
 
-list1 = np.sort(np.asarray(list1))
-list2 = np.sort(np.asarray(list2))
-difference = np.subtract(np.copy(list1), np.copy(list2))
+# Problem 1
+total = np.sum(np.abs(np.subtract(np.sort(np.asarray(list1)), np.sort(np.asarray(list2)))))
 
-total = np.subtract(np.sort(np.asarray(list1)), np.sort(np.asarray(list2)))
+# Problem 2
+similarity = np.sum(np.asarray([int(np.count_nonzero(list2 == np.int64(val))) * val for val in list1]))
 
-# print(np.sum(np.abs(difference)))
-
-similarity = 0
-
-for val in list1:
-    # print(val)
-    occurrences = 0
-    if val in list2:
-        for k in list2:
-            if k == val:
-                occurrences += 1
-    similarity += (val * occurrences)
-
+print(total)
 print(similarity)
