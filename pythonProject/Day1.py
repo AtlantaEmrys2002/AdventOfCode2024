9538cb80da27fe5e34e13c3ce7065e45107f657b
AtlantaEmrys2002 <milli@Millicents-MacBook-Air.local>
2024-12-01 23:09:16 +0000

    Making more efficient

--- pythonProject/Day1.py
+++ pythonProject/Day1.py
@@ -6,7 +6,7 @@ data = np.delete(np.genfromtxt('day1input.txt', delimiter=' '), [1, 2], 1).trans
 list1, list2 = data[0], data[1]
 
 # Problem 1
-total = np.sum(np.abs(np.subtract(np.sort(np.asarray(list1)), np.sort(np.asarray(list2)))))
+total = np.sum(np.abs(np.subtract(np.sort(list1), np.sort(list2))))
 
 # Problem 2
 similarity = np.sum(np.asarray([int(np.count_nonzero(list2 == np.int64(val))) * val for val in list1]))
