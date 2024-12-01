6e6c87b8c13454fe1644a2ce303555005641e5f7
AtlantaEmrys2002 <milli@Millicents-MacBook-Air.local>
2024-12-01 23:06:47 +0000

    Improving file read and format

--- pythonProject/Day1.py
+++ pythonProject/Day1.py
@@ -1,22 +1,9 @@
 # Import Libraries
 import numpy as np
 
-# Read File and Format Input
-file = open('day1input.txt', 'r')
-first_line = file.readline().split()
-
-list1 = [int(first_line[0])]
-list2 = [int(first_line[1])]
-
-while True:
-    first_line = file.readline().split()
-    if not first_line:
-        break
-    else:
-        line_int = first_line
-        line1, line2 = line_int[0], line_int[1]
-        list1.append(int(line1))
-        list2.append(int(line2))
+# Read Files and Format Input
+data = np.delete(np.genfromtxt('day1input.txt', delimiter=' '), [1, 2], 1).transpose()
+list1, list2 = data[0], data[1]
 
 # Problem 1
 total = np.sum(np.abs(np.subtract(np.sort(np.asarray(list1)), np.sort(np.asarray(list2)))))
